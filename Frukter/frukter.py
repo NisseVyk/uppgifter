c7d78b45bb57a3f2eb19053c6134bacf59737c7b
Nils Vykander <nivy0316@storsjogymnasiet.se>
2023-03-13 13:12:00 +0100

    halva programmet är klart

---
+++ Frukter/frukter.py
@@ -0,0 +1,11 @@
+import random
+
+frukten =("Apelsin","Banan","Kiwi","Citron","Äpple")
+
+looping = True
+
+while looping:
+    go = input("Vill du välja en frukt till")
+
+    if (go == "nej"):
+    break
