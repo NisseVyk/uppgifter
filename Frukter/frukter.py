d71dc5a8c16d85a6cd1e3789c58d32ac1c180c75
Nils Vykander <nivy0316@storsjogymnasiet.se>
2023-03-13 14:20:48 +0100

    antagligen färdigt program

--- Frukter/frukter.py
+++ Frukter/frukter.py
@@ -1,11 +1,31 @@
 import random
 
-frukten =("Apelsin","Banan","Kiwi","Citron","Äpple")
+def print_fruit(userinput):
+    fnr = int(userinput)
+    print("\n" + frukter[fnr-1] + " kommer här")
+
+frukter =("Apelsin","Banan","Kiwi","Citron","Äpple")
 
 looping = True
 
 while looping:
-    go = input("Vill du välja en frukt till")
+    i = 1
+    print("--------------------")
+    print("\n :Fruktautomat: \n")
+    for frukt in frukter:
+        print(str(i) + ": " + frukt)
+        i += 1
+
+    fruktnr = input("\nMata in siffra för vald frukt: ")
+    print_fruit(fruktnr)
+
+    go = input("Vill du välja en frukt till?\n")
+
 
     if (go == "nej"):
-    break
+        break
+print("\n----------------------------------\n")
+
+print("Förresten här får du en frukt till!")
+slumpfrukt = random.randint(1, 5)
+print_fruit(slumpfrukt)
