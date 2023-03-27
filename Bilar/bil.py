dbe186e249d2cab44b8bedbd903737650757401d
NisseVyk <nivy0316@storsjogymnasiet.se>
2023-03-27 13:21:38 +0200

    klassen är klar

---
+++ Bilar/bil.py
@@ -0,0 +1,22 @@
+class bil:
+    #constructor körs först
+    def __init__(self, fabrikat, color, lager):
+        self.fabrikat = fabrikat
+        self.color = color
+        self.lager = lager
+
+    def getFabrikat(self):
+        return self.fabrikat
+    def setFabrikat(self, fabrikat):
+        self.fabrikat = fabrikat
+
+    def setColor(self, color):
+        self.color = color
+
+    def getColor(self):
+        return self.color
+    
+    def setLager(self, lager):
+        self.lager = lager
+    def getLager(self):
+        return self.lager
